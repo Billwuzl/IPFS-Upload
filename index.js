797ed8f1c1a9cf7ea960ac00e9c37002385ad5c0
Billwuzl <73667922+Billwuzl@users.noreply.github.com>
2022-01-24 14:30:45 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import upload from './upload';
 import "./index.css";
+import generateToken from './'
 
 window.addEventListener('DOMContentLoaded', (event) => {
   const status = document.getElementById("status");
+  const address = document.getElementById("OwnerAddress").value; // not sure whether this is correct or not
   document.getElementById("input").addEventListener("change", handleFiles, false);
+ 
 
   function handleFiles() {
     const file = this.files[0];
@@ -17,6 +20,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
       const gateway = await upload(buffer);
 
       status.innerHTML = `Uploaded <a href="${gateway}" target="_blank">here</a>`;
+
+      // status.innerHTML = "Generating NFT...";
+
+      // const tokenId = await  generateToken(gateway, address); 
+
+      // status.innerHTML = `Uploaded <a href="${tokenId}" target="_blank">here</a>`
     };
   }
 });
